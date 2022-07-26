b06114fd6f2b0fd0a5bc3d2730e3d30e6f445531
Neeraj Babani <nickdev.2043@gmail.com>
2022-07-26 19:58:57 +0530

    defining winning move

--- Connect4.py
+++ Connect4.py
@@ -5,14 +5,14 @@ ROW_COUNT=6
 COLUMN_COUNT=7
 
 def create_board():
-    board =np.zeros((6,7))
+    board =np.zeros((ROW_COUNT,COLUMN_COUNT))
     return board
 
 def drop_peice(board,row,col,piece):
     board[row][col]=piece
 
 def is_valid_location(board,col):
-    return board[5][col]==0
+    return board[ROW_COUNT-1][col]==0
 
 def get_next_open_row(board,col):
     for r in  range(ROW_COUNT):
@@ -21,6 +21,20 @@ def get_next_open_row(board,col):
 
 def print_board(board):
    print( np.flip(board,0))
+
+def winning_move(board,piece):
+    #for horizontal location win 
+    for c in range (COLUMN_COUNT-3):
+        for r in range(ROW_COUNT):
+            if board[r][c]==piece and board[r][c+1]==piece and board[r][c+2]==piece and board[r][c+3]==piece :
+                return True
+    #for vertical location win
+    for c in range (COLUMN_COUNT):
+        for r in range(ROW_COUNT-3):
+            if board[r][c]==piece and board[r+1][c]==piece and board[r+2][c]==piece and board[r+3][c]==piece :
+                return True
+    
+
 
 board = create_board()
 print_board(board)
@@ -35,12 +49,23 @@ while not game_over:
             row=get_next_open_row(board,col)
             drop_peice(board,row,col,1)
 
+            if winning_move(board,1):
+                print ("____player 1 WINS !!! Congrets!!!____")
+                game_over=True
+
     #Ask for player 2 input
     else:
         col=int(input("Player 2 make your Selection(0-6):"))
         if is_valid_location(board,col):
             row=get_next_open_row(board,col)
             drop_peice(board,row,col,2)
+
+            if winning_move(board,2):
+                print ("____player 2 WINS !!! Congrets!!!____")
+                game_over=True
+
+           
+
     print_board(board)
 
     turn +=1
