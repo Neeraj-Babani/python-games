b743915d0f3bf269480f8de206a953e6a248a4ca
Neeraj Babani <nickdev.2043@gmail.com>
2022-07-27 09:39:51 +0530

    defining winning move

--- Connect4.py
+++ Connect4.py
@@ -34,6 +34,19 @@ def winning_move(board,piece):
             if board[r][c]==piece and board[r+1][c]==piece and board[r+2][c]==piece and board[r+3][c]==piece :
                 return True
     
+    #for postively sloped diaganol
+    for c in range (COLUMN_COUNT-3):
+        for r in range(ROW_COUNT-3):
+            if board[r][c]==piece and board[r+1][c+1]==piece and board[r+2][c+2]==piece and board[r+3][c+3]==piece :
+                return True
+    
+    #for negatively sloped diaganol
+    
+    for c in range (COLUMN_COUNT-3):
+        for r in range(3,ROW_COUNT):
+            if board[r][c]==piece and board[r-1][c-1]==piece and board[r-2][c+2]==piece and board[r-3][c+3]==piece :
+                return True
+    
 
 
 board = create_board()
